4b9bf2de9243b2a619ba4afbdf1f002361dbd3b9
LoidMachado <loidmachado@outlook.com>
2023-09-04 15:21:41 +0100

    Criei e acessei as props.

--- meu-projecto/src/App.js
+++ meu-projecto/src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SaymyName';
+import Pessoa from './components/Pessoa';
 function App() {
-  const name = "Loid"
-  const newName =  name.toUpperCase()
+  const name = "Loid";
+  const newName =  name.toUpperCase();
   function soma(a,b) {
-    return a+b
+    return a+b;
   }
+  const nome = 'Aline Jorgina'
   return (
     <div className="App">
         <h2>Alterando o jsx, {newName}</h2>  
         <p>{soma(2,3)}</p>    
         <HelloWorld/>
+        <SayMyName nome='Loid'/>
+        <SayMyName nome='Machado'/>
+        <SayMyName nome={nome}/>
+        <Pessoa 
+            nome='Ivanilda' 
+            idade='12' 
+            profissao='Estudante' 
+            foto='https://via.placeholder.com/150'/>
     </div>
   );
 }
---
+++ meu-projecto/src/components/Pessoa.js
@@ -0,0 +1,13 @@
+function Pessoa(props) {
+    
+    return (
+        <div>
+            <img src={props.foto} alt={props.nome}/>
+            <h2>Nome:{props.nome}</h2>
+            <p>Idade:{props.idade}</p>
+            <p>Profissão:{props.profissao}</p>
+        </div>
+    )
+}
+
+export default Pessoa
